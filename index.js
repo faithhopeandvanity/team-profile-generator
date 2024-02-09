fd5d2fd77390ecd10978d2322432191d92538f1f
faithhopeandvanity <faithhopeandvanity@gmail.com>
2024-02-09 12:17:35 +0000

    constructed constructors!

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const Manager = require("./lib/Manager.js");
+const Manager = require("./libr/Manager.js");
 const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
-//'lib' as in 'library'
-const Engineer = require("./lib/Engineer.js");
-const Intern = require("./lib/Intern.js");
+//'libr' as in 'library'
+const Engineer = require("./libr/Engineer.js");
+const Intern = require("./libr/Intern.js");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const generateTeam = require("./src/template.js")
