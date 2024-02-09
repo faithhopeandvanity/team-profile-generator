f3ec06ffacacc7699d60eb059e04992f772479fe
faithhopeandvanity <faithhopeandvanity@gmail.com>
2024-02-09 17:24:18 +0000

    tests

--- index.js
+++ index.js
@@ -6,147 +6,161 @@ const path = require("path");
 const Engineer = require("./libr/Engineer.js");
 const Intern = require("./libr/Intern.js");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-const generateTeam = require("./src/template.js")
+const outputPath = path.join(OUTPUT_DIR, "roster.html");
+const generateTeam = require("./src/boilerplate.js");
 
-function runGenerator () {
+function runGenerator() {
+    function createRoster() {
+        inquirer
+            .prompt([
+                {
+                    type: "list",
+                    message: "Who are you adding to your roster?",
+                    name: "addEmployeePrompt",
+                    choices: ["Manager", "Intern", "Engineer", "All done!"],
+                },
+            ])
+            .then(function (userInput) {
+                switch (userInput.addEmployeePrompt) {
+                    case "Manager":
+                        addManager();
+                        break;
+                    case "Intern":
+                        addInter();
+                        break;
+                    case "Engineer":
+                        addEngineer();
+                        break;
 
-  function createRoster () {
-    inquirer.prompt([{
-      type: "list",
-      message: "Who are you adding to your roster?",
-      name: "addEmployeePrompt",
-      choices: ["Manager", "Intern", "Engineer", "All done!"]
-    }]).then(function (userInput) {
-      switch(userInput.addEmployeePrompt) {
-        case "Manager":
-          addManager();
-          break;
-        case "Intern":
-          addInter();
-          break;
-        case "Engineer":
-          addEngineer();
-          break;
+                    default:
+                        htmlBuilder();
+                }
+            });
+    }
 
-        default:
-          htmlBuilder();
-      }
-    })
-  }
+    function addManager() {
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    name: "managerName",
+                    message: "What is the manager's name?",
+                },
 
-  function addManager() {
-    inquirer.prompt ([
-      
-      {
-        type: "input",
-        name: "managerName",
-        message: "What is the manager's name?"
-      },
-  
-      {
-        type: "input",
-        name: "managerIdNo",
-        message: "What is their employee ID #?"
-      },
-  
-      {
-        type: "input",
-        name: "managerEmail",
-        message: "What is their email address?"
-      },
-  
-      {
-        type: "input",
-        name: "managerOfficeNumber",
-        message: "What is their office number?"
-      }
-  
-    ]).then(answers => {
-      const manager = new Manager(answers.managerName, answers.managerIdNo, answers.managerEmail, answers.managerOfficeNumber);
-      teamArray.push(manager);
-      createRoster();
-    });
-  
-  }
+                {
+                    type: "input",
+                    name: "managerIdNo",
+                    message: "What is their employee ID #?",
+                },
 
-  function addEngineer() {
-    inquirer.prompt([
-      
-      {
-        type: "input",
-        name: "engineerName",
-        message: "What is theirname?"
-      },
+                {
+                    type: "input",
+                    name: "managerEmail",
+                    message: "What is their email address?",
+                },
 
-      {
-        type: "input",
-        name: "engineerIdNo",
-        message: "What is their employee ID #?" 
-      },
+                {
+                    type: "input",
+                    name: "managerOfficeNumber",
+                    message: "What is their office number?",
+                },
+            ])
+            .then((answers) => {
+                const manager = new Manager(
+                    answers.managerName,
+                    answers.managerIdNo,
+                    answers.managerEmail,
+                    answers.managerOfficeNumber
+                );
+                teamArray.push(manager);
+                createRoster();
+            });
+    }
 
-      {
-        type: "input",
-        name: "engineerEmail",
-        message: "What is their email address?"
-      },
+    function addEngineer() {
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    name: "engineerName",
+                    message: "What is theirname?",
+                },
 
-      {
-        type: "input",
-        name: "engineerGithub",
-        message: "What is their GitHub username?"
-      }
+                {
+                    type: "input",
+                    name: "engineerIdNo",
+                    message: "What is their employee ID #?",
+                },
 
-    ]).then(answers => {
-      const engineer = new Engineer(answers.engineerName, answers.engineerIdNo, answers.engineerEmail, answers.engineerGithub);
-      teamArray.push(engineer);
-      createRoster();
-    });
+                {
+                    type: "input",
+                    name: "engineerEmail",
+                    message: "What is their email address?",
+                },
 
-  }
-  function addIntern() {
-    inquirer.prompt([
-      
-      {
-        type: "input",
-        name: "internName",
-        message: "What is their name?"
-      },
+                {
+                    type: "input",
+                    name: "engineerGithub",
+                    message: "What is their GitHub username?",
+                },
+            ])
+            .then((answers) => {
+                const engineer = new Engineer(
+                    answers.engineerName,
+                    answers.engineerIdNo,
+                    answers.engineerEmail,
+                    answers.engineerGithub
+                );
+                teamArray.push(engineer);
+                createRoster();
+            });
+    }
+    function addIntern() {
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    name: "internName",
+                    message: "What is their name?",
+                },
 
-      {
-        type: "input",
-        name: "internIdNo",
-        message: "What is their employee ID #?" 
-      },
+                {
+                    type: "input",
+                    name: "internIdNo",
+                    message: "What is their employee ID #?",
+                },
 
-      {
-        type: "input",
-        name: "internEmail",
-        message: "What is their email address?"
-      },
+                {
+                    type: "input",
+                    name: "internEmail",
+                    message: "What is their email address?",
+                },
 
-      {
-        type: "input",
-        name: "internSchool",
-        message: "What educational institution is your intern currently at?"
-      }
+                {
+                    type: "input",
+                    name: "internInstitution",
+                    message:
+                        "What educational institution is your intern currently at?",
+                },
+            ])
+            .then((answers) => {
+                const intern = new Intern(
+                    answers.internName,
+                    answers.internIdNo,
+                    answers.internEmail,
+                    answers.internInstitution
+                );
+                teamArray.push(intern);
+                createRoster();
+            });
+    }
+    function htmlBuilder() {
+        console.log("Success! Your roster has been created.");
 
-    ]).then(answers => {
-      const intern = new Intern(answers.internName, answers.internIdNo, answers.internEmail, answers.internSchool);
-      teamArray.push(intern);
-      createRoster();
-    });
+        fs.writeFileSync(outputPath, generateTeam(teamArray), "UTF-8");
+    }
 
-  }
-  function htmlBuilder () {
-    console.log("Success! Your roster has been created.")
-
-    fs.writeFileSync(outputPath, generateTeam(teamArray), "UTF-8")
-
-}
-
-createRoster();
-
+    createRoster();
 }
 
 runGenerator();
