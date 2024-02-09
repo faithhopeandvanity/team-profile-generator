8dce80abeae1bbbb99c491624a161f39f0fa0f4f
faithhopeandvanity <faithhopeandvanity@gmail.com>
2024-02-09 11:49:00 +0000

    create roster function

--- index.js
+++ index.js
@@ -8,3 +8,29 @@ const Intern = require("./lib/Intern.js");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const generateTeam = require("./src/template.js")
+
+function runApp () {
+
+  function createRoster () {
+    inquirer.prompt([{
+      type: "list",
+      message: "Who are you adding to your roster?",
+      name: "addEmployeePrompt",
+      choices: ["Manager", "Intern", "Engineer", "All done!"]
+    }]).then(function (userInput) {
+      switch(userInput.addEmployeePrompt) {
+        case "Manager":
+          addManager();
+          break;
+        case "Intern":
+          addInter();
+          break;
+        case "Engineer":
+          addEngineer();
+          break;
+
+        default:
+          htmlBuilder();
+      }
+    })
+  }
